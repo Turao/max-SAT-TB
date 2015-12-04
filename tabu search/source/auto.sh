6b569766ec18fe210927357ccf05b7feaf67e062
andretelles84 <andretelles84@gmail.com>
2015-12-04 15:34:37 -0200

    Novos Scripts e novos testes

--- tabu search/source/auto.sh
+++ tabu search/source/auto.sh
@@ -1,11 +1,18 @@
 #/bin/bash
+p1=$1
+p2=$2
+p3=$3
+
 FILES=../data/*
-OUTPUT=../solution/
+OUTPUT=../solution/$p1.$p2.$p3/
+
+mkdir $OUTPUT
+
 for f in $FILES
 do
 	echo "Processing $f file..."
 	# take action on each file. $f store current file name
-	./maxsat < $f > $OUTPUT$(basename "$f").out
+	./maxsat $p1 $p2 $p3 < $f > $OUTPUT$(basename "$f").out
 done
 echo "Acabou. Pressione qualquer tecla."
 read
---
+++ tabu search/source/resultados.sh
@@ -0,0 +1,44 @@
+#/bin/bash
+DIRECTORIES=../solution/*/
+FILES=./*.out
+OUTPUT=../solution/resultados.csv
+
+echo "" > $OUTPUT
+
+for d in $DIRECTORIES
+do
+	pasta=$(echo $d | cut -d'/' -f 3 | cut -d'/' -f 1)
+
+	pasta1=$(echo $pasta | cut -d'.' -f 1)
+	pasta2=$(echo $pasta | cut -d'.' -f 2)
+	pasta3=$(echo $pasta | cut -d'.' -f 3)
+
+	echo -n "$pasta1;$pasta2;$pasta3;" >> $OUTPUT
+
+	for f in $d$FILES
+	do
+		filename="${f%.*}"
+		nome="${filename%.*}"
+		nome=$(basename $nome)
+
+		i=0
+
+		while IFS='' read -r line || [[ -n "$line" ]] ; do
+
+			i=$(($i + 1))
+		
+			if [ $i == 9 ]
+			then
+				solucao=$(echo "$line" | cut -d':' -f 2 | cut -d' ' -f 2 | cut -d'.' -f 1)
+			elif [ $i == 10 ]
+			then
+				tempo=$(echo "$line" | cut -d':' -f 2 | cut -d' ' -f 2 | cut -d's' -f 1)
+				echo -n "$solucao;$tempo;" >> $OUTPUT
+				break
+			fi
+		done < "$f"
+	done
+	echo "" >> $OUTPUT
+done
+echo "Acabou. Pressione qualquer tecla."
+read
